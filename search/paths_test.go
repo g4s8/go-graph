ccb4eb2e3f36b35eec1aa486a768f6a34500233a
Kirill <g4s8.public@gmail.com>
2022-05-31 13:29:51 +0300

    test: more tests for simple paths

--- search/paths_test.go
+++ search/paths_test.go
@@ -1,33 +1,101 @@
 package search
 
 import (
-	m "github.com/g4s8/go-matchers"
+	"fmt"
 	"testing"
+
+	m "github.com/g4s8/go-matchers"
 )
 
 func TestSearchPaths(t *testing.T) {
-	g := newTestGraph(5, true)
-	g.connect(0, 1)
-	g.connect(1, 2)
-	g.connect(0, 2)
-	g.connect(1, 3)
-	g.connect(2, 3)
-	g.connect(2, 4)
-	g.connect(4, 3)
-	g.connect(3, 4)
-
-	paths := SimplePaths(g, 0, 3)
-
-	assert := m.Assert(t)
-	assert.That("find 5 paths", paths, m.LenIs(5))
-	assert.That("find 0-1-2-3 path", paths,
-		m.HasItemEq(Path{0, 1, 2, 3}))
-	assert.That("find 0-1-2-4-3 path", paths,
-		m.HasItemEq(Path{0, 1, 2, 4, 3}))
-	assert.That("find 0-1-3 path", paths,
-		m.HasItemEq(Path{0, 1, 3}))
-	assert.That("find 0-2-3 path", paths,
-		m.HasItemEq(Path{0, 2, 3}))
-	assert.That("find 0-2-4-3 path", paths,
-		m.HasItemEq(Path{0, 2, 4, 3}))
+	for i, tc := range []struct {
+		size        int
+		directed    bool
+		connections [][2]int
+		path        [2]int
+		expect      [][]int
+	}{
+		{
+			size:     5,
+			directed: true,
+			connections: [][2]int{
+				{0, 1}, {1, 2}, {0, 2}, {1, 3},
+				{2, 3}, {2, 4}, {4, 3}, {3, 4},
+			},
+			path: [2]int{0, 3},
+			expect: [][]int{
+				{0, 1, 2, 3},
+				{0, 1, 2, 4, 3},
+				{0, 1, 3},
+				{0, 2, 3},
+				{0, 2, 4, 3},
+			},
+		},
+		{
+			size:     5,
+			directed: true,
+			connections: [][2]int{
+				{0, 1}, {0, 2}, {0, 3},
+				{1, 4}, {2, 4}, {3, 4},
+				{0, 4},
+			},
+			path: [2]int{0, 4},
+			expect: [][]int{
+				{0, 1, 4}, {0, 2, 4}, {0, 3, 4},
+				{0, 4},
+			},
+		},
+		{
+			size:     4,
+			directed: true,
+			connections: [][2]int{
+				{0, 1}, {0, 2},
+				{1, 3}, {2, 3},
+				{0, 3},
+				{1, 2}, {2, 1},
+			},
+			path: [2]int{0, 3},
+			expect: [][]int{
+				{0, 3},
+				{0, 1, 3}, {0, 2, 3},
+				{0, 1, 2, 3}, {0, 2, 1, 3},
+			},
+		},
+		{
+			size:     5,
+			directed: true,
+			connections: [][2]int{
+				{0, 4},
+				{0, 1}, {1, 4},
+				{0, 2}, {2, 4},
+				{0, 3}, {3, 4},
+				{1, 2}, {2, 1}, {2, 3}, {3, 2}, {1, 3}, {3, 1},
+			},
+			path: [2]int{0, 4},
+			expect: [][]int{
+				{0, 4},
+				{0, 1, 4}, {0, 2, 4}, {0, 3, 4},
+				{0, 1, 2, 4}, {0, 1, 3, 4},
+				{0, 2, 1, 4}, {0, 2, 3, 4},
+				{0, 3, 1, 4}, {0, 3, 2, 4},
+				{0, 1, 2, 3, 4}, {0, 1, 3, 2, 4},
+				{0, 2, 1, 3, 4}, {0, 2, 3, 1, 4},
+				{0, 3, 1, 2, 4}, {0, 3, 2, 1, 4},
+			},
+		},
+	} {
+		g := newTestGraph(tc.size, tc.directed)
+		for _, con := range tc.connections {
+			g.connect(con[0], con[1])
+		}
+		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
+			paths := SimplePaths(g, tc.path[0], tc.path[1])
+			assert := m.Assert(t)
+			assert.That("size", paths, m.LenIs(len(tc.expect)))
+			for _, p := range tc.expect {
+				assert.That(fmt.Sprintf("expect-%v", p),
+					paths, m.HasItemEq(Path(p)))
+			}
+		})
+	}
 }
